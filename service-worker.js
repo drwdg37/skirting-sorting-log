0024df2de0a7c22b2b89b390330f3b604a050c35
drwdg37 <eric.steve.palmer@gmail.com>
2025-08-12 19:16:50 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,36 @@
-// Cache version v10.2
-const CACHE = 'skirting-log-v10_2';
-const ASSETS = ['./','./index.html?v=10.2','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k))))); self.clients.claim(); });
+// Cache version v10.4 (iPad state fix + icon)
+const CACHE = 'skirting-log-v10_4';
+const ASSETS = [
+  './',
+  './index.html?v=10.4',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))));
+  self.clients.claim();
+});
 self.addEventListener('fetch', e => {
-  const req = e.request; const url = new URL(req.url);
-  if (req.mode === 'navigate') { e.respondWith(caches.match('./index.html?v=10.2').then(r => r || fetch('./index.html?v=10.2'))); return; }
-  e.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
-    try { if (req.method==='GET' && url.origin===self.location.origin) { caches.open(CACHE).then(c => c.put(req, res.clone())); } } catch(e){}
-    return res;
-  }).catch(() => caches.match('./index.html?v=10.2'))));
+  const req = e.request;
+  const url = new URL(req.url);
+  if (req.mode === 'navigate') {
+    e.respondWith(caches.match('./index.html?v=10.4').then(r => r || fetch('./index.html?v=10.4')));
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      try {
+        if (req.method === 'GET' && url.origin === self.location.origin) {
+          caches.open(CACHE).then(c => c.put(req, res.clone()));
+        }
+      } catch(e) {}
+      return res;
+    }).catch(() => caches.match('./index.html?v=10.4')))
+  );
 });
